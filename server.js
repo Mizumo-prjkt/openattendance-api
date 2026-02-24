5c9ca2e87966a332109a322275e70e067db5d8c6
Mizumo-prjkt <bluepearl394@gmail.com>
2026-02-24 19:33:56 +0000

    feat: Making server.js CORS compliant

--- server.js
+++ server.js
@@ -59,6 +59,16 @@ app.use(bodyParser.urlencoded({
     limit: '50mb'
 }));
 
+// CORS Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 // Initialize Socket.io
 const io = new Server({
